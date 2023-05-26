911664a417d4c27332035fabfe274cb6b1ce89d4
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-26 11:09:47 +0200

    [andriiv] enable Spring Security and Basic Authentication

---
+++ src/main/java/com/andriiv/todoappbackend/config/AuthSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.andriiv.todoappbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Created by Roman Andriiv (26.05.2023 - 10:13)
+ */
+@Configuration
+public class AuthSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}

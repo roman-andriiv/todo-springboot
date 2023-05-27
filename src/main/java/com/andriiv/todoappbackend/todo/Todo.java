419808a19832f922678f1383eb77ecb1faf28df4
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-27 21:53:43 +0200

    [andriiv] get tables ready

--- src/main/java/com/andriiv/todoappbackend/todo/Todo.java
+++ src/main/java/com/andriiv/todoappbackend/todo/Todo.java
@@ -1,5 +1,9 @@
 package com.andriiv.todoappbackend.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +16,16 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String description;
     private LocalDate targetDate;
     private boolean done;
+
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (id, description, done, target_date, username)
+values (1001, 'Learn Spring', false, CURRENT_DATE(), 'andriiv');
+
+insert into todo (id, description, done, target_date, username)
+values (1002, 'Learn AWS', false, CURRENT_DATE(), 'andriiv');
+
+insert into todo (id, description, done, target_date, username)
+values (1003, 'Learn React', false, CURRENT_DATE(), 'andriiv');
+
+insert into todo (id, description, done, target_date, username)
+values (1004, 'Learn Java', false, CURRENT_DATE(), 'andriiv');
